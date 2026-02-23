0709942fed4e28640c5caf2db1fac54089aa35dd
nithyagiri <nithyagiri27@gmail.com>
2026-02-23 14:03:34 -0600

    Project setup and Database configuration done

---
+++ cake-factory-back-end/src/main/java/org/launchcode/cake_factory_back_end/CakeFactoryBackEndApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.cake_factory_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeFactoryBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeFactoryBackEndApplication.class, args);
+    }
+}
---
+++ cake-factory-back-end/src/test/java/org/launchcode/cake_factory_back_end/CakeFactoryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.cake_factory_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
